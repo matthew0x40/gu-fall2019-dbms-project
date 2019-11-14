58563d0d00f834cf11de2a681a2b9a68b72f6907
Matthew Lee <kwwxis@users.noreply.github.com>
2019-11-13 21:35:44 -0800

    Update

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+  connectionLimit : 10,
+  host     : 'us-cdbr-iron-east-05.cleardb.net',
+  user     : 'b571a2cf11720b',
+  password : 'fd80f87f',
+  database : 'heroku_0dda02611b0f97b'
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    db.query('SELECT * FROM shows', function (error, results, fields) {
+        res.render('pages/index', {
+            testdata: JSON.stringify(results)
+        });
+    });    
+});
+
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
