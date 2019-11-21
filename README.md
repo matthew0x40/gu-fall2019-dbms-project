fcd26408a6ce255a20f6cd0b242fc99fe89c72a0
Matthew Lee <kwwxis@users.noreply.github.com>
2019-11-21 10:22:03 -0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # gu-fall2019-dbms-project
+
+Requires at least Node v10. After cloning the repository run `npm install` to install dependencies.
+
+Run `node index.js` to start the web application server locally (deploys to localhost:5000).
+Type `Ctrl + C` in terminal to stop server. You will need to stop and restart the server if you
+make changes to any code.
+
+Alternatively you can install Nodemon (via `npm install -g nodemon`) and then run `nodemon index.js`
+which will hot-reload the server without needing to manually restart.
+
+The project is deployed to gu-fall2019-dbms-project.herokuapp.com. Any pushes to this repository will
+automatically deploy to Heroku.
--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+'use strict';
+
+require('dotenv').config();
+
 const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
 const app = express();
 const router = express.Router();
 
-const mysql = require('mysql');
-
-const db = mysql.createPool({
-  connectionLimit : 10,
-  host     : 'us-cdbr-iron-east-05.cleardb.net',
-  user     : 'b571a2cf11720b',
-  password : 'fd80f87f',
-  database : 'heroku_0dda02611b0f97b'
-});
+const db = require('src/database');
+const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+router.use(function(req, res, next) {
+    res.header('X-XSS-Protection', 1);
+    next();
+});
+
+router.get('/', (req, res) => {
     db.query('SELECT * FROM shows', function (error, results, fields) {
         res.render('pages/index', {
             testdata: JSON.stringify(results)
@@ -24,5 +24,5 @@ app.get('/', (req, res) => {
     });    
 });
 
-
+app.use('/', router);
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
